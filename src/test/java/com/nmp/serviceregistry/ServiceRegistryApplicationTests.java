9c944b4f557d7a7fad32951cf737dc4e068da06e
namrathapanigrahi <nmp.1994@gmail.com>
2021-05-13 22:17:21 +0530

    SERVICE_REGISTRY(EUREKA)

---
+++ src/test/java/com/nmp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
